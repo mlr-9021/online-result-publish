de914e0533987c384553e2556995f771fc832c56
zilani <kader.cwork@gmail.com>
2020-07-18 11:04:24 +0600

    Exam Commitee add

--- student/urls.py
+++ student/urls.py
@@ -3,14 +3,19 @@ from .import views
 
 
 urlpatterns = [
-    path('create/', views.create_student, name="create-student"),
-    path('list/', views.student_list, name="student-list"),
-    path('search/', views.search_student, name="search-student"),
+    #path('create/', views.create_student, name="create-student"),
+    #path('list/', views.student_list, name="student-list"),
+    #path('search/', views.search_student, name="search-student"),
+	
+	#path('edit/<int:pk>', views.edit_student, name="edit-student"),
+
+    #path('delete/<int:student_id>', views.delete_student, name="delete-student"),
+	
 
     path('search_result/', views.search_result, name="search-result"),
+	
+	
     #path('register/', views.register_student, name="register-student"),
-    path('edit/<int:pk>', views.edit_student, name="edit-student"),
-
-    path('delete/<int:student_id>', views.delete_student, name="delete-student"),
+    
 
 ]
--- student/views.py
+++ student/views.py
@@ -9,23 +9,9 @@ from .models import StudentProfile,Result
 #from .forms import StudentInfoForm,StudentDetailInfoForm
 from .forms import SearchStudentForm, StudentProfileForm,SearchResultForm
 
-def search_student(request):
-    forms = SearchStudentForm(request.GET or None)
 
-    roll = request.GET.get('roll', None)
-    session = request.GET.get('session', None)
 
-    if roll and session:
-        students = StudentProfile.objects.filter(roll=roll, session=session)
-        print(students)
-        if students:
-            context = {'forms': forms, 'students': students}
-            return render(request, 'student/search.html', context)
-        else:
-            #messages.success(request, "please submit right information")
-            return HttpResponse("rong")
-    context = {'forms': forms}
-    return render(request, 'student/search.html', context)
+
 
 
 def search_result(request):
@@ -45,7 +31,7 @@ def search_result(request):
             context = {'forms': forms, 'students': students}
             return render(request, 'student/search_result.html', context)
         else:
-            students = {'forms': forms,'gpa':"Please Submit Valid ID and Semester Name.Try Again! "}
+            students = {'forms': forms,'gpa':"Sorry, Student ID and Semester Name Doesn't Match.Try Again! "}
             print(students)
             #context = {'forms': forms, 'students': students}
             #messages.error(request, "Successfully Created")
@@ -54,46 +40,6 @@ def search_result(request):
     context = {'forms': forms}
     return render(request, 'student/search_result.html', context)
 
-
-def student_list(request):
-    std = StudentProfile.objects.all()
-    context = {'students': std}
-    return render(request, 'student/student_list.html', context)
-
-
-
-def create_student(request):
-    forms = StudentProfileForm()
-    if request.method =='POST':
-          forms =StudentProfileForm(request.POST)
-          if forms.is_valid():
-              forms.save()
-              return redirect('student-list')
-
-    #forms = TeacherForm()
-    context = {'forms': forms}
-    return render(request, 'student/create_std.html', context)
-
-
-
-
-
-
-def edit_student(request, pk):
-    std_obj = StudentProfile.objects.get(id=pk)
-    forms = StudentProfileForm(instance=std_obj)
-    if request.method == 'POST':
-        forms = StudentProfileForm(request.POST, instance=std_obj)
-        if forms.is_valid():
-            forms.save()
-            return redirect('student-list')
-    context = {'forms': forms}
-    return render(request, 'student/edit_student.html', context)
-
-def delete_student(request, student_id):
-    student_obj = StudentProfile.objects.get(id=student_id)
-    student_obj.delete()
-    return redirect('student-list')
 
 
 
